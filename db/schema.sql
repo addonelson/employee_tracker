0229edbb8e5be55fd985559432946e6aadb21242
Aaron Donelson <aaron.donelson23@gmail.com>
2021-10-11 17:10:30 -0400

    schema and seeds built out

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS _role;
+CREATE TABLE _role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES _role(id)
+    ON DELETE SET NULL
+);
